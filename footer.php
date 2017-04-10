a047e31378bb3081ea266ba90bac45658b69ef44
Alexisenp <alexisenp@gmail.com>
2017-04-09 23:23:38 -0300

    Primer commit

---
+++ footer.php
@@ -0,0 +1,52 @@
+
+            <footer>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-1 col-xs-3 col-sm-2 logo "><a href="#"><img src="images/logo_devtic.jpg" alt=""  /></a></div>
+                        <div class="col-lg-8 col-xs-9 col-sm-5 ">Copyrights 2017, www.objetosperdidos.cl por <a href="http://www.devtic.cl"> www.devtic.cl</a></div>
+                        <div class="col-lg-3 col-xs-12 col-sm-5 sociconcent">
+                            <ul class="socialicons">
+                                <li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+
+        <!-- get jQuery from the google apis -->
+        <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.js"></script>
+
+
+        <!-- SLIDER REVOLUTION 4.x SCRIPTS  -->
+        <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.plugins.min.js"></script>
+        <script type="text/javascript" src="rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
+
+        <script src="js/bootstrap.min.js"></script>
+
+
+        <!-- LOOK THE DOCUMENTATION FOR MORE INFORMATIONS -->
+        <script type="text/javascript">
+
+            var revapi;
+
+            jQuery(document).ready(function() {
+                "use strict";
+                revapi = jQuery('.tp-banner').revolution(
+                        {
+                            delay: 15000,
+                            startwidth: 1200,
+                            startheight: 278,
+                            hideThumbs: 10,
+                            fullWidth: "on"
+                        });
+
+            });	//ready
+
+        </script>
+
+        <!-- END REVOLUTION SLIDER -->
+    </body>
+
+
+</html>
---
+++ index.php
@@ -0,0 +1,309 @@
+<?php
+include("header.php");
+?>
+
+            <section class="content">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-xs-0 col-md-4"></div>
+                        <div class="col-lg-8 col-xs-12 col-md-8">
+                            <div class="pull-left"><a href="#" class="prevnext"><i class="fa fa-angle-left"></i></a></div>
+                            <div class="pull-left">
+                                <ul class="paginationforum">
+                                    <li class="hidden-xs"><a href="#">1</a></li>
+                                    <li class="hidden-xs"><a href="#">2</a></li>
+                                    <li class="hidden-xs"><a href="#">3</a></li>
+                                    <li class="hidden-xs"><a href="#">4</a></li>
+                                    <li><a href="#">5</a></li>
+                                    <li><a href="#">6</a></li>
+                                    <li><a href="#" class="active">7</a></li>
+                                    <li><a href="#">8</a></li>
+                                    <li class="hidden-xs"><a href="#">9</a></li>
+                                    <li class="hidden-xs"><a href="#">10</a></li>
+                                    <li class="hidden-xs hidden-md"><a href="#">11</a></li>
+                                    <li class="hidden-xs hidden-md"><a href="#">12</a></li>
+                                    <li class="hidden-xs hidden-sm hidden-md"><a href="#">13</a></li>
+                                    <li><a href="#">1586</a></li>
+                                </ul>
+                            </div>
+                            <div class="pull-left"><a href="#" class="prevnext last"><i class="fa fa-angle-right"></i></a></div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-3 col-md-3">
+
+                            <!-- -->
+                            <div class="sidebarblock">
+                                <h3>Categorias</h3>
+                                <div class="divline"></div>
+                                <div class="blocktxt">
+                                    <ul class="cats">
+                                        <li><a href="#">Cosas encontradas<span class="badge pull-right">20</span></a></li>
+                                        <li><a href="#">Cosas perdidas<span class="badge pull-right">10</span></a></li>
+                                        <li><a href="#">Mascotas encontradas<span class="badge pull-right">50</span></a></li>
+                                        <li><a href="#">Mascotas perdidas <span class="badge pull-right">36</span></a></li>
+                                        <li><a href="#">Documentos encontrados <span class="badge pull-right">41</span></a></li>
+                                        <li><a href="#">Documentos perdidos <span class="badge pull-right">11</span></a></li>
+                                        <li><a href="#">Todos <span class="badge pull-right">5</span></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <!--
+                            <div class="sidebarblock">
+                                <h3>Poll of the Week</h3>
+                                <div class="divline"></div>
+                                <div class="blocktxt">
+                                    <p>Which game you are playing this week?</p>
+                                    <form action="#" method="post" class="form">
+                                        <table class="poll">
+                                            <tr>
+                                                <td>
+                                                    <div class="progress">
+                                                        <div class="progress-bar color1" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 90%">
+                                                            Call of Duty Ghosts
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td class="chbox">
+                                                    <input id="opt1" type="radio" name="opt" value="1">
+                                                    <label for="opt1"></label>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <div class="progress">
+                                                        <div class="progress-bar color2" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 63%">
+                                                            Titanfall
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td class="chbox">
+                                                    <input id="opt2" type="radio" name="opt" value="2" checked>
+                                                    <label for="opt2"></label>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <div class="progress">
+                                                        <div class="progress-bar color3" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
+                                                            Battlefield 4
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td class="chbox">
+                                                    <input id="opt3" type="radio" name="opt" value="3">
+                                                    <label for="opt3"></label>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </form>
+                                    <p class="smal">Voting ends on 19th of October</p>
+                                </div>
+                            </div>
+
+                            <!--
+                            <div class="sidebarblock">
+                                <h3>My Active Threads</h3>
+                                <div class="divline"></div>
+                                <div class="blocktxt">
+                                    <a href="#">This Dock Turns Your iPhone Into a Bedside Lamp</a>
+                                </div>
+                                <div class="divline"></div>
+                                <div class="blocktxt">
+                                    <a href="#">Who Wins in the Battle for Power on the Internet?</a>
+                                </div>
+                                <div class="divline"></div>
+                                <div class="blocktxt">
+                                    <a href="#">Sony QX10: A Funky, Overpriced Lens Camera for Your Smartphone</a>
+                                </div>
+                                <div class="divline"></div>
+                                <div class="blocktxt">
+                                    <a href="#">FedEx Simplifies Shipping for Small Businesses</a>
+                                </div>
+                                <div class="divline"></div>
+                                <div class="blocktxt">
+                                    <a href="#">Loud and Brave: Saudi Women Set to Protest Driving Ban</a>
+                                </div>
+                            </div><!-- -->
+
+
+                        </div>
+                        <div class="col-lg-9 col-md-9">
+                            <!-- POST -->
+                            <div class="post">
+                                <div class="wrap-ut pull-left">
+                                    <div class="userinfo pull-left">
+                                        <div class="imagen">
+                                            <img src="images/perroperdido.jpg" alt="" />
+                                        </div>
+
+                                        <!--
+                                        <div class="icons">
+                                            <img src="images/icon1.jpg" alt="" /><img src="images/icon4.jpg" alt="" />
+                                        </div><!-- -->
+                                    </div>
+                                    <div class="posttext pull-left">
+                                        <h2><a href="detalle.php">Perrito encontrado en las compañias</a></h2>
+                                        <p>It's one thing to subject yourself to a Halloween costume mishap because, hey, that's your prerogative.</p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="postinfo pull-left">
+                                    <div class="comments">
+                                        <div class="commentbg">
+                                            0
+                                            <div class="mark"></div>
+                                        </div>
+
+                                    </div>
+                                    <div class="views"><i class="fa fa-eye"></i> 1,568</div>
+                                    <div class="time"><i class="fa fa-clock-o"></i> 24 min</div>                                    
+                                </div>
+                                <div class="clearfix"></div>
+                            </div><!-- POST -->
+
+                            <!-- POST -->
+                            <div class="post">
+                                <div class="wrap-ut pull-left">
+                                    <div class="userinfo pull-left">
+                                        <div class="imagen">
+                                            <img src="images/1.jpg" alt="" />
+                                        </div>
+
+                                        <!--
+                                        <div class="icons">
+                                            <img src="images/icon1.jpg" alt="" /><img src="images/icon4.jpg" alt="" />
+                                        </div><!-- -->
+                                    </div>
+                                    <div class="posttext pull-left">
+                                        <h2><a href="detalle.php">10 Kids Unaware of Their Halloween Costume</a></h2>
+                                        <p>It's one thing to subject yourself to a Halloween costume mishap because, hey, that's your prerogative.</p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="postinfo pull-left">
+                                    <div class="comments">
+                                        <div class="commentbg">
+                                            0
+                                            <div class="mark"></div>
+                                        </div>
+
+                                    </div>
+                                    <div class="views"><i class="fa fa-eye"></i> 1,568</div>
+                                    <div class="time"><i class="fa fa-clock-o"></i> 24 min</div>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div><!-- POST -->
+
+                            <!-- POST -->
+                            <div class="post">
+                                <div class="wrap-ut pull-left">
+                                    <div class="userinfo pull-left">
+                                        <div class="imagen">
+                                            <img src="images/1.jpg" alt="" />
+                                        </div>
+
+                                        <!--
+                                        <div class="icons">
+                                            <img src="images/icon1.jpg" alt="" /><img src="images/icon4.jpg" alt="" />
+                                        </div><!-- -->
+                                    </div>
+                                    <div class="posttext pull-left">
+                                        <h2><a href="detalle.php">10 Kids Unaware of Their Halloween Costume</a></h2>
+                                        <p>It's one thing to subject yourself to a Halloween costume mishap because, hey, that's your prerogative.</p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="postinfo pull-left">
+                                    <div class="comments">
+                                        <div class="commentbg">
+                                            0
+                                            <div class="mark"></div>
+                                        </div>
+
+                                    </div>
+                                    <div class="views"><i class="fa fa-eye"></i> 1,568</div>
+                                    <div class="time"><i class="fa fa-clock-o"></i> 24 min</div>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div><!-- POST -->
+
+
+                            <!-- POST -->
+                            <div class="post">
+                                <div class="wrap-ut pull-left">
+                                    <div class="userinfo pull-left">
+                                        <div class="imagen">
+                                            <img src="images/1.jpg" alt="" />
+                                        </div>
+
+                                        <!--
+                                        <div class="icons">
+                                            <img src="images/icon1.jpg" alt="" /><img src="images/icon4.jpg" alt="" />
+                                        </div><!-- -->
+                                    </div>
+                                    <div class="posttext pull-left">
+                                        <h2><a href="detalle.php">10 Kids Unaware of Their Halloween Costume</a></h2>
+                                        <p>It's one thing to subject yourself to a Halloween costume mishap because, hey, that's your prerogative.</p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="postinfo pull-left">
+                                    <div class="comments">
+                                        <div class="commentbg">
+                                            0
+                                            <div class="mark"></div>
+                                        </div>
+
+                                    </div>
+                                    <div class="views"><i class="fa fa-eye"></i> 1,568</div>
+                                    <div class="time"><i class="fa fa-clock-o"></i> 24 min</div>
+                                </div>
+                                <div class="clearfix"></div>
+                            </div><!-- POST -->
+                        </div>
+                    </div>
+                </div>
+
+
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-xs-0 col-md-4"></div>
+                        <div class="col-lg-8 col-xs-12">
+                            <div class="pull-left"><a href="#" class="prevnext"><i class="fa fa-angle-left"></i></a></div>
+                            <div class="pull-left">
+                                <ul class="paginationforum">
+                                    <li class="hidden-xs"><a href="#">1</a></li>
+                                    <li class="hidden-xs"><a href="#">2</a></li>
+                                    <li class="hidden-xs"><a href="#">3</a></li>
+                                    <li class="hidden-xs"><a href="#">4</a></li>
+                                    <li><a href="#">5</a></li>
+                                    <li><a href="#">6</a></li>
+                                    <li><a href="#" class="active">7</a></li>
+                                    <li><a href="#">8</a></li>
+                                    <li class="hidden-xs"><a href="#">9</a></li>
+                                    <li class="hidden-xs"><a href="#">10</a></li>
+                                    <li class="hidden-xs hidden-md"><a href="#">11</a></li>
+                                    <li class="hidden-xs hidden-md"><a href="#">12</a></li>
+                                    <li class="hidden-xs hidden-sm hidden-md"><a href="#">13</a></li>
+                                    <li><a href="#">1586</a></li>
+                                </ul>
+                            </div>
+                            <div class="pull-left"><a href="#" class="prevnext last"><i class="fa fa-angle-right"></i></a></div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+
+
+            </section>
+<?php
+include("footer.php");
+?>
